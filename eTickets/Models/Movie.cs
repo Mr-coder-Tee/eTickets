a67c1636446d4264c218c0207620ccb6aa417063
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2022-03-16 18:43:48 +0200

    ..

---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using eTickets.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double Price { get; set; }
+        public String ImageURL { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
