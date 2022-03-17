7eff880868d8d9b48583fcac26c915069e2131a9
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2022-03-17 23:23:03 +0200

    ...

---
+++ eTickets/Controllers/CinemasController.cs
@@ -0,0 +1,24 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CinemasController(AppDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            var allCinemas = await _context.Cinemas.ToListAsync();
+            return View();
+        }
+    }
+}
--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -10,8 +10,11 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name = "Profile Picture URL")]
         public string ProfilePictureURL { get; set; }
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
+        [Display(Name = "Bio")]
         public string Bio { get; set; }
         public List<Actor_Movie> Actors_Movies { get; set; }
     }
